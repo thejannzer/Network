b64c9c237cdcd770643b9403dee7b0c567c60961
thejannzer <scholz.carlsfeld@gmail.com>
2025-12-21 22:46:04 +0100

    fixed Nummerierung

--- Stringabgleich_clean/clean_bash.sh
+++ Stringabgleich_clean/clean_bash.sh
@@ -3,16 +3,16 @@
 echo "== Debian Cleanup Script =="
 
 # --- Paket-Cache aufräumen ---
-echo "[1/6] Leere APT Cache..."
+echo "[1/3] Leere APT Cache..."
 sudo apt clean
 sudo apt autoclean
 
 # --- Nicht mehr benötigte Pakete entfernen ---
-echo "[2/6] Entferne ungenutzte Pakete..."
+echo "[2/3] Entferne ungenutzte Pakete..."
 sudo apt autoremove -y
 
 # --- Papierkorb löschen ---
-echo "[6/6] Leere den Papierkorb..."
+echo "[3/3] Leere den Papierkorb..."
 rm -rf ~/.local/share/Trash/*
 
 echo "== Cleanup abgeschlossen! 🎉 =="
